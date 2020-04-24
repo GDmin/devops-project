236852fbc276790f3889e4aff916f8354f9a696e
GDmin <minco.0418@gmail.com>
2020-04-24 18:12:40 +0800

    test ver1.0 up time

--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -1,5 +1,5 @@
 /*
-	Caminar by TEMPLATED
+	Radius by TEMPLATED
 	templated.co @templatedco
 	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
@@ -7,18 +7,19 @@
 (function($) {
 
 	skel.breakpoints({
-		xlarge: '(max-width: 1680px)',
-		large: '(max-width: 1280px)',
-		medium: '(max-width: 980px)',
-		small: '(max-width: 736px)',
-		xsmall: '(max-width: 480px)'
+		xlarge:	'(max-width: 1680px)',
+		large:	'(max-width: 1280px)',
+		medium:	'(max-width: 980px)',
+		small:	'(max-width: 736px)',
+		xsmall:	'(max-width: 480px)'
 	});
 
 	$(function() {
 
-		var	$window 	= $(window),
-			$body 		= $('body'),
-			$header 	= $('#header');
+		var	$window = $(window),
+			$body = $('body'),
+			$header = $('#header'),
+			$footer = $('#footer');
 
 		// Disable animations/transitions until the page has loaded.
 			$body.addClass('is-loading');
@@ -29,6 +30,9 @@
 				}, 100);
 			});
 
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
 		// Prioritize "important" elements on medium.
 			skel.on('+medium -medium', function() {
 				$.prioritize(
@@ -37,8 +41,39 @@
 				);
 			});
 
-		// Gallery.
-			$('.gallery').poptrox();
+		// Header.
+			$header.each( function() {
+
+				var t 		= jQuery(this),
+					button 	= t.find('.button');
+
+				button.click(function(e) {
+
+					t.toggleClass('hide');
+
+					if ( t.hasClass('preview') ) {
+						return true;
+					} else {
+						e.preventDefault();
+					}
+
+				});
+
+			});
+
+		// Footer.
+			$footer.each( function() {
+
+				var t 		= jQuery(this),
+					inner 	= t.find('.inner'),
+					button 	= t.find('.info');
+
+				button.click(function(e) {
+					t.toggleClass('show');
+					e.preventDefault();
+				});
+
+			});
 
 	});
 
